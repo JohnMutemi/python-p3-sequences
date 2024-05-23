9e6d92f4f25d615bd0895a92092aeb5802388155
JohnMutemi <jmtutorsalp@gmail.com>
2024-05-23 12:40:59 +0300

    commit fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+           print([])
+           return
+    # Initialize the Fibonacci sequence
+    fib_array = [0, 1]
+    
+    # Print the initial values based on the required length
+    if length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+    
+    # Generate the Fibonacci sequence up to the desired length
+    for i in range(2, length):
+        next_number = fib_array[-1] + fib_array[-2]
+        fib_array.append(next_number)
+    
+    # Print the Fibonacci sequence
+    print(fib_array)
+
+
+    
+
+    
